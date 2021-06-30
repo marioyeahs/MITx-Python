587fed38b7a98acaae1af77af29cca89a5c39943
Jenn <jenn@MacBook-Pro-de-Jenn.local>
2021-06-29 21:43:54 -0500

    List excersices

---
+++ Lists/Lists1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 29 21:38:02 2021
+
+@author: jenn
+"""
+
+s="I <3 CS"
+list_of_s=list(s)
+print(list_of_s)
+print(s.split('<'))
+s_again=''.join(list_of_s)
+print(s_again)
